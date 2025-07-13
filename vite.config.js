f206c040969d399924b730a0eefda3bdc5f12ade
Dagr02 <danieliusgr02@gmail.com>
2025-07-13 19:36:08 +0100

    Switched to vercel

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "https://dagr02.github.io/dagr02-portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
