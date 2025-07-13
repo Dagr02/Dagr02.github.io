76914af19a5deeae6678ec08b7c26b2dadaad932
Dagr02 <danieliusgr02@gmail.com>
2025-07-13 17:45:11 +0100

    deployment script

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Dagr02-github.io",
+  base: "dagr02-portfolio",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
