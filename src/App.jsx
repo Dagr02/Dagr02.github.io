d386b57b43b1ce8bac7e8ab78cde88baf364081b
Dagr02 <danieliusgr02@gmail.com>
2025-07-13 19:37:44 +0100

    Undone app.jsx changes via emailjs

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,12 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { NotFound } from './pages/NotFound'
 import { Home } from './pages/Home'
 import { Toaster } from './components/ui/toaster'
-import emailjs from "@emailjs/browser";
 
 function App() {
-  
-  emailjs.init(import.meta.env.VITE_EMAILJS_PUBLIC_KEY);
-
   return (
     <>
       <Toaster />
-      <BrowserRouter basename='/dagr02-portfolio'>
+      <BrowserRouter>
         <Routes>
           <Route index element={<Home />}/>
           <Route path='*' element={<NotFound />} />
